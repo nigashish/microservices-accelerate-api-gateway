646a535e84d42d24b616f29719c19c2c6e4d507d
nigashish <ashishn@eupheus.in>
2023-06-23 13:15:51 +0530

    DB Regarding  changes

--- src/main/java/com/xamcheck/accelerate/cloud/gateway/CloudGatewayApplication.java
+++ src/main/java/com/xamcheck/accelerate/cloud/gateway/CloudGatewayApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableEurekaClient
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
